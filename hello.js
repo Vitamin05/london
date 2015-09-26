992a2da29bbd1d382141586672789688e7fdea97
besartshyti <besartshyti@gmail.com>
2015-09-26 17:10:04 +0100

    create simple server

--- hello.js
+++ hello.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+
+// Our first server
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.end('Hello Regi');
+}).listen(3000, function () {
+    console.log('Server started somewhere');
+});
+
